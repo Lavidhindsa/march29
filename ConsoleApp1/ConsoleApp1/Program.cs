1897d9ad61afd6dee766c426c7959b16587063fd
Labhwinder Singh <728432@student.tor.loc>
2019-03-29 21:03:22 -0400

    hguh

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -18,10 +18,19 @@ namespace ConsoleApp1
 
             
         }
-        static void Download()
+        static async void Download()
         {
-            Thread.Sleep(3000);
-            Console.WriteLine("download complete...");
+            await Network.download();
+            Console.WriteLine("download complete");
         }
-    }
-}
+
+
+        class Network
+        {
+            static public Task download()
+            {
+                return Task.Run(
+                () => ThreadStaticAttribute.Sleep(60000));
+            }
+        }
+    } }
